0eb756b577bd231c45130459c377e6dd6e87047b
Mark Ponti <pontimgh@gmal.com>
2014-06-17 19:40:23 -0700

    Streamlining

--- quickSort.cpp
+++ quickSort.cpp
@@ -2,19 +2,29 @@
 //
 
 #include "stdafx.h"
+#include "quickSort.h"
+#include <time.h>
+#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
-void printArray(int* array, int n);
-void QuickSort(int* array, int start, int end);
-int partition(int* array, int pivotValue, int start, int end);
-void swap(int &a, int &b);
+const int ARRAY = 10;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int values[ARRAY];
+	srand(time(NULL));
 
+	for (int i = 0; i < ARRAY; i++)
+	{
+		values[i] = rand() % 101;
+	}
 
+	printArray(values, 8);
+	QuickSort(values, 0, 7);
+	cout << "The array has been sorted." << endl;
+	printArray(values, 8);
 	cin.get();
 	return 0;
 }
@@ -51,5 +61,29 @@ void QuickSort(int* array, int start, int end)
 		QuickSort(array, start, splitPoint - 1);
 		QuickSort(array, splitPoint + 1, end);
 	}
+}
+
+int partition(int* array, int pivot, int start, int end)
+{
+	int left = start; //our left boundry (start of partition we're sorting)
+	int right = end; //our right boundry (end of partition we're soring)
+
+	while (left < right) // move our pivot until the boundries meet
+	{
+		while (pivot < array[right] && right > left) //keep moving until a lesser element is found or until the boundries are equal
+		{
+			right--; //move left (towards the beginning of our array)
+		}
+		swap(array[left], array[right]); //after finding a lesser element we swap it with our pivot
+
+		//keep moving until a value greater than or equal to our pivot is 
+		//found or our left boundry equals our right
+		while (pivot >= array[left] && left < right)
+		{
+			left++; //move right (towards the end of our array)
+		}
+		swap(array[right], array[left]); //when a value that is greater than or equal to our pivot we swap them and stop iterating
+	}
+	return left; //return the split point (where our pivot is)
 }
 
---
+++ quickSort.h
@@ -0,0 +1,5 @@
+
+void printArray(int* array, int n);
+void QuickSort(int* array, int startIndex, int endIndex);
+int partition(int* array, int pivotValue, int startIndex, int endIndex);
+void swap(int &a, int &b);
